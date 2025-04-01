79ade5cb0a2335135db09db9980c775dc71cec67
Jon Keatley <me@jon-keatley.com>
2025-04-02 00:06:34 +0100

    rewrote main methods, I think I have everything needed to start processing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wasm-fasta
 
-A wasm wrapper for noodles providing fasta features
+A wasm wrapper for noodles providing fasta features for compressing and creating a index.
 
 Project template taken from https://github.com/trunk-rs/trunk/tree/main/examples/vanilla
 
@@ -8,8 +8,13 @@ Todo
 - [ ] Gain a understanding of Noodles
 - [ ] Create a method that compresses a provided fasta file
 - [ ] Create a method that generates a index for a provided fasta file
-- [ ] Create a javascript interface to the above methods
+- [ ] Create a interface to the above methods
 - [ ] Create a HTML example application
+- [ ] Add CI/CD to deploy example page
+- [ ] Add more useful information here
+
+## Improvements
+- Pass file to WASM so it can create a file reader and iterate over the file rather than loading it into memory
 
 ## Building
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#![recursion_limit = "1024"]
 
 use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::*;
-use web_sys::window;
+use noodles::fasta::Reader; 
+//use web_sys::window;
 
 fn start_app() {
     //let document = window()
@@ -12,6 +12,10 @@ fn start_app() {
     //let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
     //body.append_child(text_node.as_ref())
     //    .expect("Failed to append text");
+    
+    //build download buttons
+    
+    //on clicks need to pull the desired values
 }
 
 #[wasm_bindgen]
@@ -19,11 +23,49 @@ pub fn wasm_hello() {
     web_sys::console::log_1(&"Hello from WASM!".into());
 }
 
+fn create_compressed_fasta(fasta:&str) -> Result<String, String>
+{
+    let fasta_in:String = fasta.to_owned();
+    let fasta_bytes = fasta_in.into_bytes();
+    let mut reader = Reader::new(fasta_bytes.as_slice());
+    
+    for rec_result in reader.records()
+    {
+        
+    }
+    
+    Err("WIP".to_string())
+}
+
+fn create_fasta_index(fasta:&str) -> Result<String, String>
+{
+    let fasta_in:String = fasta.to_owned();
+    let fasta_bytes = fasta_in.into_bytes();
+    let mut reader = Reader::new(fasta_bytes.as_slice());
+    
+    for rec_result in reader.records()
+    {
+        
+    }
+
+    
+    Err("WIP".to_string())
+}
+
 #[wasm_bindgen]
 pub fn process_file_as_string(f: String)
 {
+    
     web_sys::console::log_1(&"WASM got this!".into());
-    web_sys::console::log_1(&f.into());
+    //web_sys::console::log_1(&f.into());
+    
+    //let fasta_bytes = f.into_bytes().clone();
+    //let repo_results =  get_repo(fasta_bytes.as_slice());
+    
+    //process input into Repository
+    // enable download buttons
+    // store fasta somewhere
+    // onclick of buttons download index or bz zipped fasta
 }
 
 fn main() {
